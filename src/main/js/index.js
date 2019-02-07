2cca5a78161eb78923198145caa0f3f360ee297f
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2019-02-07 11:44:03 +0100

    use new extension point for author suffix

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -3,4 +3,4 @@ import React from "react";
 import { binder } from "@scm-manager/ui-extensions";
 import PushlogCommitter from "./PushlogCommitter";
 
-binder.bind("changesets.changeset.author.metadata", PushlogCommitter);
+binder.bind("changesets.author.suffix", PushlogCommitter);
